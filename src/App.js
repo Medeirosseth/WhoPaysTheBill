419f9268c79ed5b5542e0bb4244867920cbffba0
Seth <Medeirosseth@gamil.com>
2022-09-22 12:42:41 -0700

    styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { useContext } from "react";
+import { MyContext } from "./Context";
+import Stage1 from "./Components/stage1";
+import Stage2 from "./Components/stage2";
+import "./style/app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
+  const context = useContext(MyContext);
+  console.log(context);
   return (
     <>
-      <div>
-        <div>hello</div>
+      <div className="wrapper">
+        <div className="centerWrapper">
+          <h1>Who Pays The Bill?</h1>
+          {context.state.stage === 1 ? <Stage1 /> : <Stage2 />}
+        </div>
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { MyProvider } from "./Context/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <MyProvider>
+      <App />
+    </MyProvider>
+  </React.StrictMode>
+);
